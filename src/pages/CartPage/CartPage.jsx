a30c84904fb26a989f82189193553330acf9d372
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-23 23:49:46 +1100

    utilised cart items list container

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,5 +1,27 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContextProvider/CartContextProvider";
+import CartItemsList from "../../containers/CartItemsList/CartItemsList";
+
 const CartPage = () => {
-  return <main>Cart</main>;
+  const { cart, setCart } = useContext(CartContext);
+
+  return (
+    <main>
+      <h2>Cart</h2>
+
+      {cart && <CartItemsList items={cart} />}
+
+      {/* {cart &&
+        cart.map((item) => (
+          <>
+            <p>{item.productId}</p>
+            <p>{item.format}</p>
+            <p>{item.qty}</p>
+            <p>{item.totalPrice}</p>
+          </>
+        ))} */}
+    </main>
+  );
 };
 
 export default CartPage;
