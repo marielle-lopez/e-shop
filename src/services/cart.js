cb44b17840b562d47c23d0c063ce746211d15a6f
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-24 21:58:54 +1100

    created a function to add items to cart in database

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,4 +1,10 @@
-import { getDocs, collection } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "../config/firebase";
 
 export const getAllCartItems = async () => {
@@ -13,4 +19,35 @@ export const getAllCartItems = async () => {
   console.log(data);
 
   return data;
+};
+
+export const addItemToCart = async (
+  id,
+  title,
+  artist,
+  format,
+  img,
+  price,
+  qty,
+  inStockQty
+) => {
+  const docRef = await addDoc(collection(db, "cart"), {
+    productId: id,
+    title: title,
+    artist: artist,
+    format: format,
+    img: img,
+    unitPrice: price,
+    totalPrice: price * qty,
+    qty: qty,
+    inStockQty: inStockQty,
+  });
+
+  console.log(`Item with ID ${docRef.id} added to cart.`);
+};
+
+export const removeItemFromCart = async (cartItemId) => {
+  await deleteDoc(doc(db, "cart", cartItemId));
+
+  console.log(`Cart item with ID ${cartItemId} removed from cart.`);
 };
