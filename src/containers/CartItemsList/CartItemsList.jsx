008c10af3fb0e29afe95a422f2172e4fe7493ec1
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-24 22:00:34 +1100

    adjusted passed props and added functionality to bin button

--- src/containers/CartItemsList/CartItemsList.jsx
+++ src/containers/CartItemsList/CartItemsList.jsx
@@ -1,5 +1,7 @@
 import styles from "./CartItemsList.module.scss";
 import CartItemCard from "../../components/CartItemCard/CartItemCard";
+import { removeItemFromCart } from "../../services/cart";
+import { adjustProductQty } from "../../services/products";
 
 const CartItemsList = ({ items }) => {
   return (
@@ -9,15 +11,30 @@ const CartItemsList = ({ items }) => {
           <div className={styles.card_wrapper}>
             <CartItemCard
               key={item.id}
-              id={item.id}
+              productId={item.productId}
               title={item.title}
+              artist={item.artist}
               format={item.format}
               img={item.img}
               qty={item.qty}
-              unitPrice={item.price}
+              unitPrice={item.unitPrice}
               totalPrice={item.totalPrice}
             />
-            <button className={styles.remove_btn}>
+            <button
+              onClick={() => {
+                removeItemFromCart(item.id);
+                adjustProductQty(
+                  item.productId,
+                  item.format,
+                  item.img,
+                  false,
+                  item.unitPrice,
+                  item.inStockQty,
+                  item.inStockQty + item.qty
+                );
+              }}
+              className={styles.remove_btn}
+            >
               <img
                 className={styles.bin_icon}
                 src="../src/assets/bin.png"
