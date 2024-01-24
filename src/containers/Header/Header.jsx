a9604de1c1c21f57515833436d9ea33f980ddd51
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-25 00:59:04 +1100

    implemented escape hatch design pattern

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.logo}>
-        S
-        <img
-          className={styles.logo__icon}
-          src="./src/assets/vinyl.png"
-          alt="Vinyl icon"
-        />
-        und
-      </h1>
+      <Link className={styles.logo__link} to="/">
+        <h1 className={styles.logo}>
+          S
+          <img
+            className={styles.logo__icon}
+            src="./src/assets/vinyl.png"
+            alt="Vinyl icon"
+          />
+          und
+        </h1>
+      </Link>
       <NavBar />
     </header>
   );
