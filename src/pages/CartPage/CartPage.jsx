fa0a073c1eeed48177732084b90e5294270873a2
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-24 18:58:58 +1100

    removed commented code and applied PaddingWrapper

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,26 +1,18 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContextProvider/CartContextProvider";
 import CartItemsList from "../../containers/CartItemsList/CartItemsList";
+import PaddingWrapper from "../../containers/PaddingWrapper/PaddingWrapper";
 
 const CartPage = () => {
   const { cart, setCart } = useContext(CartContext);
 
   return (
-    <main>
-      <h2>Cart</h2>
-
-      {cart && <CartItemsList items={cart} />}
-
-      {/* {cart &&
-        cart.map((item) => (
-          <>
-            <p>{item.productId}</p>
-            <p>{item.format}</p>
-            <p>{item.qty}</p>
-            <p>{item.totalPrice}</p>
-          </>
-        ))} */}
-    </main>
+    <PaddingWrapper>
+      <main>
+        <h2>Cart</h2>
+        {cart && <CartItemsList items={cart} />}
+      </main>
+    </PaddingWrapper>
   );
 };
 
