82812a2cbbd2851c0785aee56c9569b49f2a9adf
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-24 18:56:29 +1100

    increased reusability of Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,46 @@
 import styles from "./Button.module.scss";
 
-const Button = ({ text }) => {
-  return <button className={styles.btn}>{text}</button>;
+const Button = ({ text, variant, size, handleClick, fnParams = null }) => {
+  let btnVariant = variant === "primary" ? styles.primary : styles.secondary;
+  let btnSize;
+
+  if (variant === "primary") {
+    btnVariant = styles.primary;
+  }
+
+  if (variant === "secondary") {
+    btnVariant = styles.secondary;
+  }
+
+  if (variant === "inactive") {
+    btnVariant = styles.inactive;
+  }
+
+  if (size === "small") {
+    btnSize = styles.small;
+  }
+
+  if (size === "medium") {
+    btnSize = styles.medium;
+  }
+
+  if (size === "large") {
+    btnSize = styles.large;
+  }
+
+  const stylesArr = [styles.btn, btnVariant, btnSize];
+
+  return (
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        handleClick(fnParams);
+      }}
+      className={stylesArr.join(" ")}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
